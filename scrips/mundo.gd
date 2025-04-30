f96e75c3facd868d5bb624241c829e5199acaeb9
IsaidOchoa <isaid3472@gmail.com>
2025-04-30 17:19:11 -0600

    interiores 1.2 update

--- scrips/mundo.gd
+++ scrips/mundo.gd
@@ -8,3 +8,16 @@ func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Ve
 	if event is InputEventMouseButton and event.pressed:
 		print("hola")
 		get_tree().change_scene_to_file("res://escenas/casa interior.tscn")
+		
+
+func _on_cruce_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://escenas/Interior_cruce.tscn")
+
+func _on_escuela_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://escenas/escuela_interior.tscn")
+
+func _on_edificio_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://escenas/edificio_interior.tscn")
